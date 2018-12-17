cc917c4f8e6810786a828c907a2f804d4ed7a83d
Clark A <clarka@gmail.com>
2018-12-17 16:13:52 -0700

    initial commit

---
+++ start.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    if (req.url == '/incoming') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.finput.path;
+            var newpath = '/tmp/' + files.finput.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="incoming" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="finput"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+}).listen(8080);
