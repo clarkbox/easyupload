9aecbb4ee93565198e4e9d61e996ef81f1eac00c
Clark A <clarka@gmail.com>
2018-12-17 23:28:50 -0700

    based on file not path

--- start.js
+++ start.js
@@ -1,25 +1,36 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
-    if (req.url == '//incoming') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
+
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+
+        var havefile = false;
+        if(typeof files.finput !== 'undefined' ){
+            havefile = true;
+        }else{
+            havefile = false;
+        }
+
+        if (havefile) {
             var oldpath = files.finput.path;
-            var newpath = '/tmp/repo/' + files.finput.name;
+            var newpath = '/var/www/repo/' + files.finput.name;
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
                 res.write('File uploaded');
                 res.end();
             });
-        });
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<!-- req url ' + req.url + ' -->');
-        res.write('<form action="incoming" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="finput"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
-    }
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(req.url)
+            res.write('<form action="./incoming" method="post" enctype="multipart/form-data">');
+            res.write('<input type="file" name="finput"><br>');
+            res.write('<input type="submit">');
+            res.write('</form>');
+            return res.end();
+        }
+
+    });
 }).listen(8080);
