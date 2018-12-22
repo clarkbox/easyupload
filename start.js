5730d0923d0a1a9eb863a5f9225c45d4c50407b6
Clark A <clarka@gmail.com>
2018-12-21 17:21:08 -0700

    dont do so much

--- start.js
+++ start.js
@@ -3,34 +3,16 @@ var formidable = require('formidable');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         if(typeof files.finput !== 'undefined' ){
-
             var newpath = '/var/www/repo/' + files.finput.name;
             var oldpath = files.finput.path;
-
-            function moveFile(){
-                fs.rename(oldpath, newpath, function (err) {
-                    if (err) console.log('there was an error');
-                    res.write('File uploaded');
-                    res.end();
-                });
-            }
-
-            var fn = 'na';
-
-            if(files.finput.name.indexOf('admin') > -1){
-                fn = 'admin';
-            }else if(files.finput.name.indexOf('webstore') > -1){
-                fn = 'webstore';
-            }
-
-            fs.copyFile(oldpath, '/var/www/repo/'+fn+'-latest.tgz.gpg', function(){
-                moveFile();
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) console.log('there was an error');
+                res.write('File uploaded');
+                res.end();
             });
-
         }else{
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(req.url)
@@ -40,6 +22,5 @@ http.createServer(function (req, res) {
             res.write('</form>');
             return res.end();
         }
-
     });
 }).listen(8080);
