4c3a06b44f7ef04884a841fd8ad7d311a6746172
Clark A <clarka@gmail.com>
2018-12-21 18:40:43 -0700

    parse args
    --help is your friend

--- start.js
+++ start.js
@@ -1,13 +1,23 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2));
+
+if(args.help){
+    console.log('easyupload --port 8080 --savepath /var/www/repo/ --formfileinputname finput');
+    return
+}
+
+var port = args.port || 8080;
+var savePath = args.savepath || '/var/www/repo/';
+var formFileInputName = args.formfileinputname || 'finput';
 
 http.createServer(function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        if(typeof files.finput !== 'undefined' ){
-            var newpath = '/var/www/repo/' + files.finput.name;
-            var oldpath = files.finput.path;
+        if(typeof files[formFileInputName] !== 'undefined' ){
+            var newpath = savePath + files[formFileInputName].name;
+            var oldpath = files[formFileInputName].path;
             fs.rename(oldpath, newpath, function (err) {
                 if (err) console.log('there was an error');
                 res.write('File uploaded');
@@ -16,11 +26,11 @@ http.createServer(function (req, res) {
         }else{
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(req.url)
-            res.write('<form action="incoming" method="post" enctype="multipart/form-data">');
-            res.write('<input type="file" name="finput"><br>');
+            res.write('<form action="upload" method="post" enctype="multipart/form-data">');
+            res.write('<input type="file" name="'+formFileInputName+'"><br>');
             res.write('<input type="submit">');
             res.write('</form>');
             return res.end();
         }
     });
-}).listen(8080);
+}).listen( port );
